62a841992e1edae76a00caa6f544d1b9045a5428
Bipul Jain <bipuljain44@gmail.com>
2016-02-28 01:14:10 +0530

    Adding more changes to logic and some clean up

--- news_feed/config.py
+++ news_feed/config.py
@@ -1,6 +1,6 @@
 categories = ["Music", "Movies", "Fashion", "Games", "Photography", "Career", "AI","Start Ups"]
 news_feed_text = ["I work at MindTickle", "Pune is beautiful and startup friendly",
-                  "I am smart, skilled and hard working",
+                  "I am skilled and hard working",
                   "Savours Python and Java. And work with Web, Search and Artificial Intelligence",
                   "At flipkart, managed, scaled and fine-tuned search and discovery.",
                   "Boston Dynamics are being jerks to robots, for science http://tnw.me/MMrwoGQ",
